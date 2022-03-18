b1f6ed1004858d171eaba41aaa4dd3a9ba5b40ca
Edding <renkomba@email.wm.edu>
2022-03-17 21:30:59 -0400

    added event listener & function to change <select> label on change

--- Calendar/main.js
+++ Calendar/main.js
@@ -1,3 +1,15 @@
+/* -----BANNER----- */
+let staffView = document.querySelector('#staff-view');
+let staff = staffView.querySelector('select');
+const changeLabel = () => {
+    let selectedStaff = staff.value;
+    let label = staffView.querySelector('label');
+    let text = label.dataset.ogLabel;
+    
+    label.innerHTML = selectedStaff === 'general' ? text : `${text} de `;
+}
+
+
 /* -----TIME----- */
 let [hours, time] = [
     23-6,
@@ -8,20 +20,20 @@ let [hours, time] = [
 // the .hour <div>. And then set the hour nums/data attributes
 const setTime = () => {
     setMinutes();
-
+    
     let hourDiv = document.querySelector('.hour');
-
+    
     while (document.querySelectorAll('.hour').length < hours) {
         time.appendChild(hourDiv.cloneNode(true));
     }
-
+    
     setHours();
 }
 
 // for each .minute, set its innerHTML to the proper num
 const setMinutes = () => {
     let minutes = document.querySelectorAll('.minute');
-
+    
     for (let i=1; i<minutes.length; i++) {
         let minute = minutes[i];
         minute.innerHTML = parseInt(minutes[i-1].innerHTML)+5;
@@ -32,7 +44,7 @@ const setMinutes = () => {
 // then set the innerHTML equal to the data attribute
 const setHours = () => {
     let hourDivs = document.querySelectorAll('[data-hour]');
-
+    
     for (let i=0; i<hours; i++) {
         hourDivs[i].dataset.hour = parseInt(hourDivs[i].dataset.hour) + i;
         hourDivs[i].innerHTML = hourDivs[i].dataset.hour;
@@ -41,33 +53,34 @@ const setHours = () => {
 
 /* VERTICAL NAVBAR */
 // const setNav = () => {
-//     let [month, navList, day, days] = [
-//         document.querySelector('#month'),
-//         document.querySelector('ul'),
-//         document.querySelector('[data-day]'),
-//         document.querySelectorAll('[data-day]')
-//     ];
-    
-//     let dayNum = parseInt(day.dataset.day);
-    
-//     day.innerHTML = dayNum;
-//     month.innerHTML = month.dataset.monthFirst;
-    
-//     while (dayNum !== 9) {
-//         navList.appendChild(document.querySelector('[data-day]').cloneNode(true));
-//         dayNum = dayNum === 30 ? 1 : dayNum+1;
-//     }
-// }
-
-// const setDays = () => {
-//     let days = document.querySelectorAll('[data-day]');
-
-//     for (let i=0; i<days.length; i++) {
-//         days[i].dataset.day = parseInt(days[i].dataset.day) + i;
-//         days[i].innerHTML = days[i].dataset.day;
-//     }
-// }
-
+    //     let [month, navList, day, days] = [
+        //         document.querySelector('#month'),
+        //         document.querySelector('ul'),
+        //         document.querySelector('[data-day]'),
+        //         document.querySelectorAll('[data-day]')
+        //     ];
+        
+        //     let dayNum = parseInt(day.dataset.day);
+        
+        //     day.innerHTML = dayNum;
+        //     month.innerHTML = month.dataset.monthFirst;
+        
+        //     while (dayNum !== 9) {
+            //         navList.appendChild(document.querySelector('[data-day]').cloneNode(true));
+            //         dayNum = dayNum === 30 ? 1 : dayNum+1;
+            //     }
+            // }
+            
+            // const setDays = () => {
+                //     let days = document.querySelectorAll('[data-day]');
+                
+                //     for (let i=0; i<days.length; i++) {
+                    //         days[i].dataset.day = parseInt(days[i].dataset.day) + i;
+                    //         days[i].innerHTML = days[i].dataset.day;
+                    //     }
+                    // }
+                    
 /* -----FUNCTIONS----- */
+document.addEventListener('change', changeLabel);
 setTime();
-setNav();
+// setNav();
